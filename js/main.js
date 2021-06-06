32252c795dbb77b712f821dc815868b3fa8af97d
Shkurinskaya Irina <ShkurinskayaIrina@gmail.com>
2021-06-06 18:24:42 +0300

    Подключен main.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const getRandomIntNumber = function(minNumber,maxNumber){
+  if (minNumber>maxNumber){
+    const buff = minNumber;
+    minNumber=maxNumber;
+    maxNumber=buff;
+  }
+  const rendomNumber=Math.floor(Math.random()*(maxNumber-minNumber+1)+minNumber);
+  console.log(rendomNumber);
+  return rendomNumber;
+};
+getRandomIntNumber(1,30);
+
+const getRandomFractionNumber = function(minNumber,maxNumber,numberOfDecimals){
+  if (minNumber>maxNumber){
+    const buff = minNumber;
+    minNumber=maxNumber;
+    maxNumber=buff;
+  }
+  const rendomNumber=Math.random()*(maxNumber-minNumber)+minNumber;
+  const roundRendomNumber=Number(rendomNumber.toFixed(numberOfDecimals));
+  return roundRendomNumber;
+};
+getRandomFractionNumber(0,1,3);
