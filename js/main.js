db02a3916670bdbc05eabbdbf40dde18af4a410e
Shkurinskaya Irina <ShkurinskayaIrina@gmail.com>
2021-06-18 19:38:13 +0300

    7.9. Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-import {SIMILAR_ADVERTISEMENT_COUNT} from './utils/data.js';
-import {createAdvertisement} from './utils/util.js';
-
-const similarAdvertisement = new Array(SIMILAR_ADVERTISEMENT_COUNT).fill(null).map(()=>createAdvertisement());
-similarAdvertisement;
+import './utils/data.js';
+import './utils/popup.js';
+//const similarAdvertisement = new Array(SIMILAR_ADVERTISEMENT_COUNT).fill(null).map(()=>createAdvertisement());
+//similarAdvertisement;
