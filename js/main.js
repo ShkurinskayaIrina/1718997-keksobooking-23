fd7e1123906fdf4a140de55cd88eaade878426a5
Shkurinskaya Irina <ShkurinskayaIrina@gmail.com>
2021-06-22 23:29:16 +0300

    8.10. Правда или действие (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import './utils/data.js';
 import './utils/popup.js';
+import './utils/advertisement-form.js';
 //const similarAdvertisement = new Array(SIMILAR_ADVERTISEMENT_COUNT).fill(null).map(()=>createAdvertisement());
 //similarAdvertisement;
