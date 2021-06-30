b463f11be9ee875e9a44e937a776726648e588ed
Shkurinskaya Irina <ShkurinskayaIrina@gmail.com>
2021-06-30 07:26:52 +0300

    8.11. Правда или действие (часть 2)

--- js/utils/advertisement-form.js
+++ js/utils/advertisement-form.js
@@ -2,11 +2,18 @@ const MIN_HEADER_LENGTH = 30;
 const MAX_HEADER_LENGTH = 100;
 const MAX_PRICE_NIGHT = 1000000;
 
+const MIN_PRICE_OF_TYPE = {
+  bungalow : 0,
+  flat : 1000,
+  hotel : 3000,
+  house : 5000,
+  palace : 10000,
+};
 
 const noticeBlock = document.querySelector('.notice');
-
 const headerInput = noticeBlock.querySelector('#title');
-headerInput.addEventListener('input',()=>{
+
+const onHeaderInput = function (){
   const valueLength = headerInput.value.length;
   if (valueLength<MIN_HEADER_LENGTH){
     headerInput.setCustomValidity(`Поле должно состоять минимум из 30 символов. Добавьте eщё ${  MIN_HEADER_LENGTH - valueLength } симв.`);
@@ -16,85 +23,77 @@ headerInput.addEventListener('input',()=>{
     headerInput.setCustomValidity('');
   }
   headerInput.reportValidity();
-});
-
-const priceInput = noticeBlock.querySelector('#price');
-priceInput.addEventListener('input',()=>{
-  if (priceInput.value>MAX_PRICE_NIGHT){
-    priceInput.setCustomValidity('Максимально возможная цена за ночь - 1 000 000 руб.');
-  } else {
-    priceInput.setCustomValidity('');
-  }
-  priceInput.reportValidity();
-});
+};
 
 const rooms = noticeBlock.querySelector('#room_number');
 const capacity = noticeBlock.querySelector('#capacity');
 const capacityList = capacity.querySelectorAll('option');
 
-rooms.addEventListener('change',()=>{
-  for ( let ii=0 ; ii < capacityList.length; ii++ ){
+const onRoomsChange = function(){
+  for ( let i=0 ; i < capacityList.length; i++ ){
     if (Number(rooms.value)===100) {
-      if (ii===3){
-        capacityList[ii].selected=true;
-        capacityList[ii].classList.remove('hidden');
+      if (i===3){
+        capacityList[i].selected=true;
+        capacityList[i].classList.remove('hidden');
       } else {
-        capacityList[ii].classList.add('hidden');
+        capacityList[i].classList.add('hidden');
       }
     } else {
       capacityList[3-Number(rooms.value)].selected=true;
-      if (ii>=(3-Number(rooms.value)) && ii<=2){
-        capacityList[ii].classList.remove('hidden');
+      if (i>=(3-Number(rooms.value)) && i<=2){
+        capacityList[i].classList.remove('hidden');
       } else {
-        capacityList[ii].classList.add('hidden');
+        capacityList[i].classList.add('hidden');
       }
     }
   }
-});
+};
 
 const typeHousing = noticeBlock.querySelector('#type');
 const priceNight = noticeBlock.querySelector('#price');
 let minPriceNight = 1000;
 
 const checkTypeHousing = function(){
-  if (typeHousing.value==='bungalow'){
-    minPriceNight = 0;
-  } else if (typeHousing.value==='flat'){
-    minPriceNight=1000;
-  } else if(typeHousing.value==='hotel'){
-    minPriceNight=3000;
-  } else if(typeHousing.value==='house'){
-    minPriceNight=5000;
-  } else if(typeHousing.value==='palace'){
-    minPriceNight=10000;
-  }
+  minPriceNight= MIN_PRICE_OF_TYPE[typeHousing.value];
   priceNight.placeholder=minPriceNight;
   priceNight.min=minPriceNight;
 };
 
 const checkPriceNight = function(){
-  if (priceNight.value<minPriceNight){
-    priceNight.setCustomValidity(`Минимально возможная цена за ночь - ${minPriceNight} руб.`);
+  if (priceNight.value>MAX_PRICE_NIGHT){
+    priceNight.setCustomValidity('Максимально возможная цена за ночь - 1 000 000 руб.');
   } else {
-    priceNight.setCustomValidity('');
+    if (priceNight.value<minPriceNight){
+      priceNight.setCustomValidity(`Минимально возможная цена за ночь - ${minPriceNight} руб.`);
+    } else {
+      priceNight.setCustomValidity('');
+    }
   }
   priceNight.reportValidity();
 };
 
-
-typeHousing.addEventListener('change',()=>{
+const onTypeHousingChange = function(){
   checkTypeHousing();
-
   checkPriceNight();
-});
+};
 
-priceNight.addEventListener('input',()=>{
+const onPriceNightInput = function(){
   checkPriceNight();
-});
+};
 
 const timeIn = noticeBlock.querySelector('#timein');
 const timeOut = noticeBlock.querySelector('#timeout');
-timeIn.addEventListener('change',()=>{
+
+const onTimeInChange = function(){
   timeOut.value=timeIn.value;
 //не пойму, надо ли блокировать выбор другого значения в timeOut?
-});
+};
+
+
+headerInput.addEventListener('input', onHeaderInput);
+priceNight.addEventListener('input', onPriceNightInput);
+
+rooms.addEventListener('change', onRoomsChange);
+typeHousing.addEventListener('change', onTypeHousingChange);
+
+timeIn.addEventListener('change',onTimeInChange);
