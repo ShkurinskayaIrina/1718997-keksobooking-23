038091fcc6a9e5779fca3b46f45453140a3dc0ee
Shkurinskaya Irina <ShkurinskayaIrina@gmail.com>
2021-07-06 13:36:10 +0300

    11.13. Надо подкачаться

--- js/utils/map.js
+++ js/utils/map.js
@@ -1,16 +1,25 @@
-import {removeFormDisabled} from './form.js';
-import {createAdvertisement, SIMILAR_ADVERTISEMENT_COUNT} from './data.js';
+import {removeFormDisabled , formAd, showErrorMessage, showSuccessMessage} from './form.js';
 import {createPopup} from './popup.js';
+import {sendData} from './api.js';
+
 const LAT_MAIN = 35.6895;
 const LNG_MAIN = 139.692;
+const buttonReset = document.querySelector('.ad-form__reset');
 
 const addAddress = function(lat,lng){
   const addressMainPinMarker = document.querySelector('#address');
   addressMainPinMarker.value = `${lat.toFixed(5)}, ${lng.toFixed(5)}`;
 };
 
+const resetMainPinMarker = function(){
+  mainPinMarker.setLatLng({
+    lat: LAT_MAIN,
+    lng: LNG_MAIN,
+  });
+};
+
 const map = L.map('map-canvas')
-  .on('load', (evt) => {
+  .on('load', () => {
     removeFormDisabled();
     addAddress(LAT_MAIN,LNG_MAIN);
   })
@@ -45,6 +54,7 @@ const mainPinMarker = L.marker(
 
 mainPinMarker.addTo(map);
 
+
 mainPinMarker.on('moveend', (evt) => {
   const newLat = evt.target.getLatLng().lat;
   const newLng = evt.target.getLatLng().lng;
@@ -53,37 +63,65 @@ mainPinMarker.on('moveend', (evt) => {
 
 const markerGroup = L.layerGroup().addTo(map);
 
-const createMarker = (element) => {
-  const {lat, lng} = element.location;
-  const icon = L.icon({
-    iconUrl: '/img/pin.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-  });
-
-  const marker = L.marker(
-    {
-      lat,
-      lng,
-    },
-    {
-      icon,
-    },
-  );
-
-  marker
-    .addTo(markerGroup)
-    .bindPopup(
-      createPopup(element),
+const createMarker = (elements) => {
+  elements.forEach((element) => {
+    const {lat, lng} = element.location; //5 знаков после зпт надо
+    const icon = L.icon({
+      iconUrl: '/img/pin.svg',
+      iconSize: [40, 40],
+      iconAnchor: [20, 40],
+    });
+    const marker = L.marker(
       {
-        keepInView: true,
+        lat,
+        lng,
+      },
+      {
+        icon,
       },
     );
+    marker
+      .addTo(markerGroup)
+      .bindPopup(
+        createPopup(element),
+        {
+          keepInView: true,
+        },
+      );
+  });
 };
 
-const similarAdvertisement = new Array(SIMILAR_ADVERTISEMENT_COUNT).fill(null).map(()=>createAdvertisement());
-//console.log(similarAdvertisement);
-similarAdvertisement.forEach((advertisement) => {
-  createMarker(advertisement);
- });
- //markerGroup.remove();
+
+const resetAdvertisement = function(){
+  const formMapFilters = document.querySelector('.map__filters');
+  formMapFilters.reset();
+  formAd.reset();
+  resetMainPinMarker();
+  addAddress(LAT_MAIN, LNG_MAIN);
+};
+
+const addAdvertisement = function(){
+  // при успешной отправке формы или при очищении формы
+  resetAdvertisement();
+  showSuccessMessage();
+};
+
+
+buttonReset.addEventListener('click', (evt) => {
+  //клик по кнопке очистить
+  evt.preventDefault();
+  resetAdvertisement();
+});
+
+const formAdSubmit = (onSuccess) =>{
+    formAd.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      sendData(
+        () => onSuccess(),
+        () => showErrorMessage(),
+        new FormData(evt.target),
+        );
+      });
+  };
+
+export {createMarker, formAdSubmit, addAdvertisement};
